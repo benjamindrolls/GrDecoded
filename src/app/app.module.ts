5bf279c8388866109c263cca438a77434aade5ce
ben <benjamindrolls@gmail.com>
2020-03-05 20:59:43 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoogleMapsModule } from '@angular/google-maps';
-import { GmapComponent } from './gmap/gmap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GmapComponent
   ],
   imports: [
     BrowserModule,
