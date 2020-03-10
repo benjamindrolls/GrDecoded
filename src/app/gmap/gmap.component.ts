25db57990f93281d77db1294b08c06b69c76f1c5
ben <benjamindrolls@gmail.com>
2020-03-09 21:34:00 -0400

    pushing so i can pull master

--- src/app/gmap/gmap.component.ts
+++ src/app/gmap/gmap.component.ts
@@ -11,9 +11,8 @@ import { ParkingMarkersService } from '../parking-markers.service';
 export class GmapComponent implements OnInit {
 
   constructor(private parkingMark: ParkingMarkersService) { }
-  @ViewChild(GoogleMap, { static: false }) map: GoogleMap
   @ViewChild(MapInfoWindow, { static: false }) info: MapInfoWindow
-  
+
   zoom = 15
   Map: google.maps.Map
   center: google.maps.LatLngLiteral
@@ -24,9 +23,71 @@ export class GmapComponent implements OnInit {
     maxZoom: 17,
     minZoom: 8,
   }
-
-  markers = [];
-  infoContent = ''
+  infoContent = '<h1>hey testing</h1>'
+  iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+  parkingAreas = [{
+    position: new google.maps.LatLng(42.960337, -85.675730),
+    info: 'hey there buddy',
+    options: {
+      icon: this.iconBase + 'parking_lot_maps.png'
+    }
+  }, {
+    position: new google.maps.LatLng(42.964024, -85.670190),
+    type: 'parking',
+    info: 'hey there son',
+    options: {
+      icon: this.iconBase + 'parking_lot_maps.png'
+    }
+  }, {
+    position: new google.maps.LatLng(42.964664, -85.672933),
+    type: 'parking',
+    info: this.infoContent,
+    options: {
+      icon: this.iconBase + 'parking_lot_maps.png'
+    }
+  }, {
+    position: new google.maps.LatLng(42.966231, -85.669396),
+    type: 'parking',
+    options: {
+      icon: this.iconBase + 'parking_lot_maps.png'
+    }
+  }, {
+    position: new google.maps.LatLng(42.966292, -85.672906),
+    type: 'parking',
+    options: {
+      icon: this.iconBase + 'parking_lot_maps.png'
+    }
+  }, {
+    position: new google.maps.LatLng(42.960251, -85.672147),
+    type: 'parking',
+    options: {
+      icon: this.iconBase + 'parking_lot_maps.png'
+    }
+  }, {
+    position: new google.maps.LatLng(42.961245, -85.671555),
+    type: 'parking',
+    options: {
+      icon: this.iconBase + 'parking_lot_maps.png'
+    }
+  }, {
+    position: new google.maps.LatLng(42.961642, -85.670597),
+    type: 'parking',
+    options: {
+      icon: this.iconBase + 'parking_lot_maps.png'
+    }
+  }, {
+    position: new google.maps.LatLng(42.959801, -85.669478),
+    type: 'parking',
+    options: {
+      icon: this.iconBase + 'parking_lot_maps.png'
+    }
+  }, {
+    position: new google.maps.LatLng(42.961518, -85.674047),
+    type: 'parking',
+    options: {
+      icon: this.iconBase + 'parking_lot_maps.png',
+    }
+  },];
 
   ngOnInit() {
       this.center = {
@@ -35,77 +96,80 @@ export class GmapComponent implements OnInit {
       }
     
 
-    let iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    // let iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
 
-    let parkingAreas = [
-      {
-        position: new google.maps.LatLng(42.960337, -85.675730),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.964024, -85.670190),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.964664, -85.672933),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.966231, -85.669396),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.966292, -85.672906),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.960251, -85.672147),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.961245, -85.671555),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.961642, -85.670597),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.959801, -85.669478),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.961518, -85.674047),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png',
-        }
-      },
-    ];
+    // let parkingAreas = [
+    //   {
+    //     position: new google.maps.LatLng(42.960337, -85.675730),
+    //     info: 'hey there buddy',
+    //     options: {
+    //       icon: iconBase + 'parking_lot_maps.png'
+    //     }
+    //   }, {
+    //     position: new google.maps.LatLng(42.964024, -85.670190),
+    //     type: 'parking',
+    //     info: 'hey there son',
+    //     options: {
+    //       icon: iconBase + 'parking_lot_maps.png'
+    //     }
+    //   }, {
+    //     position: new google.maps.LatLng(42.964664, -85.672933),
+    //     type: 'parking',
+    //     info: this.infoContent,
+    //     options: {
+    //       icon: iconBase + 'parking_lot_maps.png'
+    //     }
+    //   }, {
+    //     position: new google.maps.LatLng(42.966231, -85.669396),
+    //     type: 'parking',
+    //     options: {
+    //       icon: iconBase + 'parking_lot_maps.png'
+    //     }
+    //   }, {
+    //     position: new google.maps.LatLng(42.966292, -85.672906),
+    //     type: 'parking',
+    //     options: {
+    //       icon: iconBase + 'parking_lot_maps.png'
+    //     }
+    //   }, {
+    //     position: new google.maps.LatLng(42.960251, -85.672147),
+    //     type: 'parking',
+    //     options: {
+    //       icon: iconBase + 'parking_lot_maps.png'
+    //     }
+    //   }, {
+    //     position: new google.maps.LatLng(42.961245, -85.671555),
+    //     type: 'parking',
+    //     options: {
+    //       icon: iconBase + 'parking_lot_maps.png'
+    //     }
+    //   }, {
+    //     position: new google.maps.LatLng(42.961642, -85.670597),
+    //     type: 'parking',
+    //     options: {
+    //       icon: iconBase + 'parking_lot_maps.png'
+    //     }
+    //   }, {
+    //     position: new google.maps.LatLng(42.959801, -85.669478),
+    //     type: 'parking',
+    //     options: {
+    //       icon: iconBase + 'parking_lot_maps.png'
+    //     }
+    //   }, {
+    //     position: new google.maps.LatLng(42.961518, -85.674047),
+    //     type: 'parking',
+    //     options: {
+    //       icon: iconBase + 'parking_lot_maps.png',
+    //     }
+    //   },
+    // ];
 
 
-    for (let i = 0; i < parkingAreas.length; i++) {
-      this.markers.push(parkingAreas[i])
-    };
+    // for (let i = 0; i < parkingAreas.length; i++) {
+    //   this.markers.push(parkingAreas[i])
+    // };
 
+    
   }
 
 
@@ -117,9 +181,10 @@ export class GmapComponent implements OnInit {
     if (this.zoom > this.options.minZoom) this.zoom--
   }
 
-
-  test() {
-    console.log("hello")
+  openInfo(marker: MapMarker, content) {
+    this.infoContent = content
+    this.info.open(marker)
   }
+  
   
 }
