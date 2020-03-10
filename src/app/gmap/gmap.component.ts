a1aa420b721da49ca14388925f7955a2f079efb9
Spencer Deering <sdeering397@gmail.com>
2020-03-09 21:28:42 -0400

    created service for parking data

--- src/app/gmap/gmap.component.ts
+++ src/app/gmap/gmap.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps'
 import { ParkingMarkersService } from '../parking-markers.service';
+import { Parking } from '../parking';
 
 
 @Component({
@@ -10,12 +11,13 @@ import { ParkingMarkersService } from '../parking-markers.service';
 })
 export class GmapComponent implements OnInit {
 
-  constructor(private parkingMark: ParkingMarkersService) { }
+  park: Parking[];
+
+  constructor(private service: ParkingMarkersService) { }
   @ViewChild(GoogleMap, { static: false }) map: GoogleMap
   @ViewChild(MapInfoWindow, { static: false }) info: MapInfoWindow
   
   zoom = 15
-  Map: google.maps.Map
   center: google.maps.LatLngLiteral
   options: google.maps.MapOptions = {
     zoomControl: true,
@@ -25,7 +27,6 @@ export class GmapComponent implements OnInit {
     minZoom: 8,
   }
 
-  markers = [];
   infoContent = ''
 
   ngOnInit() {
@@ -33,79 +34,7 @@ export class GmapComponent implements OnInit {
         lat: 42.963220,
         lng:-85.667900
       }
-    
-
-    let iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-
-    let parkingAreas = [
-      {
-        position: new google.maps.LatLng(42.960337, -85.675730),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.964024, -85.670190),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.964664, -85.672933),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.966231, -85.669396),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.966292, -85.672906),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.960251, -85.672147),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.961245, -85.671555),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.961642, -85.670597),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.959801, -85.669478),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png'
-        }
-      }, {
-        position: new google.maps.LatLng(42.961518, -85.674047),
-        type: 'parking',
-        options: {
-          icon: iconBase + 'parking_lot_maps.png',
-        }
-      },
-    ];
-
-
-    for (let i = 0; i < parkingAreas.length; i++) {
-      this.markers.push(parkingAreas[i])
-    };
-
+      this.park = this.service.getMarkers();
   }
 
 
