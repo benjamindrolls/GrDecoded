3ea7c5acd3d11cb6697eaf928bbf86382e811231
Jordan Gaza <Gaza>
2020-03-25 02:21:23 -0400

    Adding restaurant service and interface to possibly add more functionality

---
+++ src/app/restaurant.ts
@@ -0,0 +1,10 @@
+export interface Restaurant {
+    name: string,
+    address: string,
+    position: google.maps.LatLng,
+    site: string,
+    options: {
+        icon: string
+        }
+    
+}
