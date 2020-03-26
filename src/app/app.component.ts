7bd458d9100602fb398efa3c06e555cea76ccec6
Jordan Gaza <Gaza>
2020-03-25 20:21:48 -0400

    Added team's githubs on the bottom of the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,14 @@ export class AppComponent implements OnInit {
     
   }
 
+  dropdown() {
+    let menu = document.getElementById("dropdown-menu");
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  }
 }
   
 
